dd2a3db26078f860e3b21c27e626e1e89d34dfb6
henrietta <piotr.maslanka@henrietta.com.pl>
2014-05-30 15:46:26 +0200

    power metering: more readouts, counter state each hour saved

--- installation/__init__.py
+++ installation/__init__.py
@@ -6,12 +6,14 @@ from morda.settings import ZERO_CONNECT
 class Install(BaseTasklet):
     
     def on_startup(self):
+        print("Installing")
         self.zero = Zero(ZERO_CONNECT)
     
         def done():
+            print ("Zero series defined")
             Tasklet.me().terminate()
     
-        installs = NCounter(7, done)
+        installs = NCounter(13, done)
     
         # define electricity
         
@@ -24,7 +26,17 @@ class Install(BaseTasklet):
         e_w1 = SeriesDefinition('pwr.phase1.power', 1, 1, 16070400, 4, 'slabsize=65536', 0)
         e_w2 = SeriesDefinition('pwr.phase2.power', 1, 1, 16070400, 4, 'slabsize=65536', 0)
         e_w3 = SeriesDefinition('pwr.phase3.power', 1, 1, 16070400, 4, 'slabsize=65536', 0)
+    
+        e_va1 = SeriesDefinition('pwr.phase1.apparent_power', 1, 1, 16070400, 4, 'slabsize=65536', 0)
+        e_va2 = SeriesDefinition('pwr.phase2.apparent_power', 1, 1, 16070400, 4, 'slabsize=65536', 0)
+        e_va3 = SeriesDefinition('pwr.phase3.apparent_power', 1, 1, 16070400, 4, 'slabsize=65536', 0)
+
+        e_var1 = SeriesDefinition('pwr.phase1.reactive_power', 1, 1, 16070400, 4, 'slabsize=65536', 0)
+        e_var2 = SeriesDefinition('pwr.phase2.reactive_power', 1, 1, 16070400, 4, 'slabsize=65536', 0)
+        e_var3 = SeriesDefinition('pwr.phase3.reactive_power', 1, 1, 16070400, 4, 'slabsize=65536', 0)
         
-        self.zero.updateDefinition(e_v1, installs()).updateDefinition(e_v2, installs()).updateDefinition(e_v3, installs()).updateDefinition(e_w1, installs()).updateDefinition(e_w2, installs()).updateDefinition(e_w3, installs()).updateDefinition(e_pc, installs())
+        
+        
+        self.zero.updateDefinition(e_v1, installs()).updateDefinition(e_v2, installs()).updateDefinition(e_v3, installs()).updateDefinition(e_w1, installs()).updateDefinition(e_w2, installs()).updateDefinition(e_w3, installs()).updateDefinition(e_pc, installs()).updateDefinition(e_va1, installs()).updateDefinition(e_va2, installs()).updateDefinition(e_va3, installs()).updateDefinition(e_var1, installs()).updateDefinition(e_var2, installs()).updateDefinition(e_var3, installs())
         
         
--- power/__init__.py
+++ power/__init__.py
@@ -21,10 +21,24 @@ class ElectricityReader(BaseTasklet):
         self.w_w2 = WriteAssistant('pwr.phase2.power', zero).start()
         self.w_w3 = WriteAssistant('pwr.phase3.power', zero).start()        
         self.w_pk = WriteAssistant('pwr.wh_counter', zero).start()
+        self.w_va1 = WriteAssistant('pwr.phase1.apparent_power', zero).start()
+        self.w_va2 = WriteAssistant('pwr.phase2.apparent_power', zero).start()
+        self.w_va3 = WriteAssistant('pwr.phase3.apparent_power', zero).start()        
+        self.w_var1 = WriteAssistant('pwr.phase1.reactive_power', zero).start()
+        self.w_var2 = WriteAssistant('pwr.phase2.reactive_power', zero).start()
+        self.w_var3 = WriteAssistant('pwr.phase3.reactive_power', zero).start()        
         
         def on_485_tasklet_open(tlet):
             self.h485 = tlet
             Timer.repeat(60, self.called_each_minute)
+            
+            # We need to ensure that this gets called on exactly full hour
+            seconds_passed_since_fullhour = int(time.time()) % 3600 
+            seconds_remaining_to_fullhour = 3600 - seconds_passed_since_fullhour
+            
+            Timer.schedule(seconds_remaining_to_fullhour, 
+                lambda: (self.called_each_hour(), Timer.repeat(3600, self.called_each_hour)))
+            
             self.refresh_data()
         
         def on_485_tasklet_id(tid):
@@ -58,12 +72,50 @@ class ElectricityReader(BaseTasklet):
             if repl == None: return
             Catalog.scatter({'pwr.wh_counter': int(repl)}, catname='values')
         
-        nc = NCounter(3, self.refresh_data)
+        def read_apparent_power(repl):
+            if repl == None: return
+            w1, a, w2, b, w3 = repl
+            Catalog.scatter({'pwr.phase1.apparent_power': w1,
+                             'pwr.phase2.apparent_power': w2,
+                             'pwr.phase3.apparent_power': w3}, catname='values')
+  
+        def read_reactive_power(repl):
+            if repl == None: return
+            w1, a, w2, b, w3 = repl
+            Catalog.scatter({'pwr.phase1.reactive_power': w1,
+                             'pwr.phase2.reactive_power': w2,
+                             'pwr.phase3.reactive_power': w3}, catname='values')            
+    
+            
+        def read_frequencies(repl):
+            if repl == None: return
+            h1, a, h2, b, h3 = repl
+            Catalog.scatter({'pwr.phase1.frequency': h1,
+                             'pwr.phase2.frequency': h2,
+                             'pwr.phase3.frequency': h3}, catname='values')            
+            
+        nc = NCounter(6, self.refresh_data)
         
         self.h485.send_sync(('read-registers', 31, 1, 5), nc(read_voltages))
         self.h485.send_sync(('read-registers', 31, 19, 5), nc(read_powers))
+        self.h485.send_sync(('read-registers', 31, 27, 5), nc(read_apparent_power))
+        self.h485.send_sync(('read-registers', 31, 35, 5), nc(read_reactive_power))
+        self.h485.send_sync(('read-registers', 31, 51, 5), nc(read_frequencies))
         self.h485.send_sync(('read-lte', 31, 79), nc(read_kwh))
         
+        
+    def called_each_hour(self):
+        # Drop the power counter into DB
+        
+        def on_gathered(data):
+            timestamp = int(time.time())
+            try:
+                self.w_pk.write(timestamp, struct.pack('d', data['pwr.wh_counter']))
+            except struct.error:
+                pass
+                
+        Catalog.gather(['pwr.wh_counter'], on_gathered, catname='values')
+
     def called_each_minute(self):
         # Aggregate values and DB'em
         
@@ -76,8 +128,17 @@ class ElectricityReader(BaseTasklet):
                 self.w_w1.write(timestamp, struct.pack('f', data['pwr.phase1.power']))
                 self.w_w2.write(timestamp, struct.pack('f', data['pwr.phase2.power']))
                 self.w_w3.write(timestamp, struct.pack('f', data['pwr.phase3.power']))
+                self.w_va1.write(timestamp, struct.pack('f', data['pwr.phase1.apparent_power']))
+                self.w_va2.write(timestamp, struct.pack('f', data['pwr.phase2.apparent_power']))
+                self.w_va3.write(timestamp, struct.pack('f', data['pwr.phase3.apparent_power']))
+                self.w_var1.write(timestamp, struct.pack('f', data['pwr.phase1.reactive_power']))
+                self.w_var2.write(timestamp, struct.pack('f', data['pwr.phase2.reactive_power']))
+                self.w_var3.write(timestamp, struct.pack('f', data['pwr.phase3.reactive_power']))
             except struct.error:
                 pass
                     
         Catalog.gather(['pwr.phase1.voltage', 'pwr.phase2.voltage', 'pwr.phase3.voltage', 
-                        'pwr.phase1.power', 'pwr.phase2.power', 'pwr.phase3.power'], on_gathered, catname='values')
+                        'pwr.phase1.power', 'pwr.phase2.power', 'pwr.phase3.power',
+                        'pwr.phase1.apparent_power', 'pwr.phase2.apparent_power',
+                        'pwr.phase3.apparent_power', 'pwr.phase1.reactive_power',
+                        'pwr.phase2.reactive_power', 'pwr.phase3.reactive_power'], on_gathered, catname='values')
